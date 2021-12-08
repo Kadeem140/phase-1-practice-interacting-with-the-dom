904b2cfd1de63b0293b94b6cee4d2d000924a337
Kadeem140 <Beckford.kadeem@yahoo.com>
2021-12-07 19:48:44 -0500

    Almost done

--- js/challenge.js
+++ js/challenge.js
@@ -3,8 +3,13 @@ const counter = document.querySelector('#counter')
 const minus = document.querySelector('#minus')
 const plus = document.querySelector('#plus')
 const pause = document.querySelector('#pause')
+const form = document.querySelector('#comment-form')
+const heart = document.querySelector('#heart')
+const likes = document.querySelector('.likes')
 
+//Valuables
 let intervalID;
+let likesCounter = {}
 
 
 //EventListeners
@@ -16,8 +21,22 @@ window.addEventListener(
 minus.addEventListener("click", () => counter.innerHTML--)
 plus.addEventListener("click", () => counter.innerHTML++)
 pause.addEventListener("click", () => {    clearInterval(intervalID)  })
+heart.addEventListener("click", (e) => {
+    const counterText = counter.innerHTML;
+    likes.append(counterText);
+    const newLi = document.createElement("li");
+    newLi.id = counterText;
+    let updatedLike = document.getElementById(counterText)
+    if(likesCounter[counterText]){ 
+        likesCounter[counterText] = 1;
+        updatedLike.innerText = `This ${counterText} has been liked ${likesCounter[counterText]} times`;
+        likes.append(newLi)
+    }
+    
+})
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    const formValue = e.target[0].value
+    document.body.append(formValue)
+    form.reset()})
 
-function pauseFunc(){
-    pause.innerHTML = "resume",
-     clearInterval(intervalID)
-}
